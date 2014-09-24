e41997476f969153b6cc59eab967fe7715017e07
Brian M. Patenaude <brianpatenaude@mac.com>
2014-09-24 14:59:27 -0700

    cleaning interpert motion

--- interpret_motion_parameters/interpret_motion_parameters.cpp
+++ interpret_motion_parameters/interpret_motion_parameters.cpp
@@ -20,52 +20,58 @@ using namespace std;
 void usage(){
     
     cout<<"\n Usage : "<<endl;
-    cout<<"interpret_motion_parameters <list of analysis directories> \n"<<endl;
+    cout<<"interpret_motion_parameters <output_file> <list of analysis directories> \n"<<endl;
     
 }
-void calc_motion( const string & fmotion, const string & brain_mask, ifstream* fout)
+void calc_motion( const string & fmotion,const string & fmotion_disp ofstream & fout)
 {
     
-    ifstream fin(argv[1]);
+    ifstream fin(fmotion.c_str());
     //read_in_brain_mask
-    volume<float> mask;
-    read_volume(mask,argv[2]);
-    
-    float xdim=mask.xdim();
-    float ydim=mask.ydim();
-    float zdim=mask.zdim();
-    int xs=mask.xsize();
-    int ys=mask.ysize();
-    int zs=mask.zsize();
-    float max_dist=0;
-    for (int x=0;x<xs;++x)
-        for (int y=0;y<ys;++y)
-            for (int z=0; z<zs;++z)
-                if (mask.value(x,y,z)!=0)
-                {
-                    float xp=x*xdim;
-                    float yp=y*ydim;
-                    float zp=z*zdim;
-                    float dist=sqrtf(xp*xp+ yp*yp + zp*zp);
-                    if (max_dist<dist) max_dist=dist;
-                    
-                }
-    
+//    volume<float> mask;
+//    read_volume(mask,brain_mask);
+//    
+//    float xdim=mask.xdim();
+//    float ydim=mask.ydim();
+//    float zdim=mask.zdim();
+//    int xs=mask.xsize();
+//    int ys=mask.ysize();
+//    int zs=mask.zsize();
+//    float max_dist=0;
+//    for (int x=0;x<xs;++x)
+//        for (int y=0;y<ys;++y)
+//            for (int z=0; z<zs;++z)
+//                if (mask.value(x,y,z)!=0)
+//                {
+//                    float xp=x*xdim;
+//                    float yp=y*ydim;
+//                    float zp=z*zdim;
+//                    float dist=sqrtf(xp*xp+ yp*yp + zp*zp);
+//                    if (max_dist<dist) max_dist=dist;
+//                    
+//                }
+//    
     
     string line;
     // vector<float> tx,ty,tz,theta,phi,psi;
     int count=0;
+//    float mean_tx=0,mean_ty=0,mean_tz=0,mean_theta=0,mean_phi=0,mean_psi=0;
+//    float max_tx=1e-16,max_ty=1e-16,max_tz=1e-16,max_theta=1e-16,max_phi=1e-16,max_psi=1e-16;
+//    float mean_motion=0;
     float mean_tx=0,mean_ty=0,mean_tz=0,mean_theta=0,mean_phi=0,mean_psi=0;
     float max_tx=1e-16,max_ty=1e-16,max_tz=1e-16,max_theta=1e-16,max_phi=1e-16,max_psi=1e-16;
-    float mean_motion;
-    
+    float mean_motion=0;
+
     while ( getline(fin,line) ) {
         //six parameters
         // cout<<"line "<<line<<endl;
         stringstream ss;
         ss<<line;
         float tx,ty,tz,theta,phi,psi;
-        ss>>tx>>ty>>tz>>theta>>phi>>psi;
+//        ss>>tx>>ty>>tz>>theta>>phi>>psi;
+        
+        ss>>theta>>phi>>psi>>tx>>ty>>tz;
+
         mean_tx+=fabs(tx);
         mean_ty+=fabs(ty);
         mean_tz+=fabs(tz);
@@ -93,10 +99,10 @@ void calc_motion( const string & fmotion, const string & brain_mask, ifstream* f
     
     
     // insert code here...
-    std::cout <<mean_motion<<" ";
-    std::cout <<mean_tx<<" "<<mean_ty<<" "<<mean_tz<<" "<<mean_theta<<" "<<mean_phi<<" "<<mean_psi<<" ";
-    std::cout <<max_tx<<" "<<max_ty<<" "<<max_tz<<" "<<max_theta<<" "<<max_phi<<" "<<max_psi<<endl;
-    std::cout <<mean_theta*max_dist<<" "<<mean_phi*max_dist<<" "<<mean_psi*max_dist<<" "<<max_theta*max_dist<<" "<<max_phi*max_dist<<" "<<max_psi*max_dist<<" "<<max_dist<<endl;
+    fout <<mean_motion<<",";
+    fout <<mean_tx<<","<<mean_ty<<","<<mean_tz<<","<<mean_theta<<","<<mean_phi<<","<<mean_psi<<",";
+   fout <<max_tx<<","<<max_ty<<","<<max_tz<<","<<max_theta<<","<<max_phi<<","<<max_psi<<",";
+    fout <<mean_theta*max_dist<<","<<mean_phi*max_dist<<","<<mean_psi*max_dist<<","<<max_theta*max_dist<<","<<max_phi*max_dist<<","<<max_psi*max_dist<<","<<max_dist<<endl;
 
     
     
@@ -105,12 +111,30 @@ void calc_motion( const string & fmotion, const string & brain_mask, ifstream* f
 
 int main (int argc, const char * argv[])
 {
-    if (argc < 1)
+    if (argc < 3)
     {
         usage();
         return 0;
     }
-  
-    return 0;
+    ofstream fout(argv[1]);
+    fout <<"mean_motion"<<",";
+    fout <<"mean_tx"<<","<<"mean_ty"<<","<<"mean_tz"<<","<<"mean_theta"<<","<<"mean_phi"<<","<<"mean_psi"<<",";
+    fout <<"max_tx"<<","<<"max_ty"<<","<<"max_tz"<<","<<"max_theta"<<","<<"max_phi"<<","<<"max_psi"<<",";
+    fout <<"mean_theta*max_dist"<<","<<"mean_phi*max_dist"<<","<<"mean_psi*max_dist"<<","<<"max_theta*max_dist"<<","<<"max_phi*max_dist"<<","<<"max_psi*max_dist"<<","<<"max_dist"<<endl;
+    if (fout.is_open())
+    {
+        int index=2;
+        while (index < argc)
+        {
+            string base_image=string(argv[index]) + "/example_func";
+            string motion_pars=string(argv[index]) + "/mc/prefiltered_func_data_mcf.par.txt";
+            cout<<"calculate motion "<<base_image<<" "<<motion_pars<<endl;
+        calc_motion(motion_pars,base_image,fout);
+    
+                fout.close();
+            ++index;
+        }
+    }
+                return 0;
 }
 
