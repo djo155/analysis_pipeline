0ad4f5f7ebc6c978c959b575c5e45b1a776bb5dc
Brian Work <brian@c02k34mvdty3.gateway.pace.com>
2014-09-26 07:15:14 -0700

    update

--- create_subject_report/create_subject_report.cpp
+++ create_subject_report/create_subject_report.cpp
@@ -23,50 +23,12 @@ void usage(){
     cout<<"create_subject_report <output_file> <list of analysis directories> \n"<<endl;
     
 }
-void calc_motion( const string & subject, const string & fmotion,const string & fmotion_disp, const string & fmotion_disp_rel, ofstream & fout)
+void calc_motion( const string & fmotion_disp, const string & fmotion_disp_rel, float & mean_disp, float & max_disp, unsigned int &  N_disp_gt_05 )
 {
-    
-
-    float mean_tx=0,mean_ty=0,mean_tz=0,mean_theta=0,mean_phi=0,mean_psi=0, mean_disp=0,mean_disp_rel=0;
-    float max_tx=1e-16,max_ty=1e-16,max_tz=1e-16,max_theta=1e-16,max_phi=1e-16,max_psi=1e-16, max_disp=1e-16, max_disp_rel=1e-16;
-
-    
-//do .par file
-    {
-        ifstream fin(fmotion.c_str());
-        
-        string line;
-        int count=0;
-        while ( getline(fin,line) ) {
-            //six parameters
-            stringstream ss;
-            ss<<line;
-            float tx,ty,tz,theta,phi,psi;
-            ss>>theta>>phi>>psi>>tx>>ty>>tz;
-            mean_tx+=fabs(tx);
-            mean_ty+=fabs(ty);
-            mean_tz+=fabs(tz);
-            mean_theta+=fabs(theta);
-            mean_phi+=fabs(phi);
-            mean_psi+=fabs(psi);
-            if (max_tx< tx) max_tx = tx;
-            if (max_ty< ty) max_ty = ty;
-            if (max_tz< tz) max_tz = tz;
-            if (max_theta< theta) max_theta = theta;
-            if (max_phi< phi) max_phi = phi;
-            if (max_psi< psi) max_psi = psi;
-            ++count;
-        }
-        fin.close();
-        
-        mean_tx/=count;
-        mean_ty/=count;
-        mean_tz/=count;
-        mean_theta/=count;
-        mean_phi/=count;
-        mean_psi/=count;
-    }
-    //do mean displacement
+    float thresh=0.5;
+    mean_disp=0.0;
+    max_disp=1e-16;
+    N_disp_gt_05=0;
     
     //do .abs file
     {
@@ -79,13 +41,12 @@ void calc_motion( const string & subject, const string & fmotion,const string & 
             ss<<line;
             float disp;
             ss>>disp;
-            mean_disp+=fabs(disp);
+            mean_disp+=disp;
             if (max_disp< disp) max_disp = disp;
             ++count;
         }
+        mean_disp /= count;
         fin_disp.close();
-        
-        mean_disp/=count;
     }
     //do .rel file
     {
@@ -98,23 +59,13 @@ void calc_motion( const string & subject, const string & fmotion,const string & 
             ss<<line;
             float disp_rel;
             ss>>disp_rel;
-            mean_disp_rel+=fabs(disp_rel);
-            if (max_disp_rel< disp_rel) max_disp_rel = disp_rel;
-            ++count;
+            if (disp_rel > thresh)
+            ++N_disp_gt_05;
         }
         fin_disp.close();
-        
-        mean_disp_rel/=count;
     }
 
-    
-    
-    // insert code here...
-    fout <<subject<<",";
-    fout <<mean_disp<<","<<max_disp<<",";
-    fout <<mean_disp_rel<<","<<max_disp_rel<<",";
-    fout <<mean_tx<<","<<mean_ty<<","<<mean_tz<<","<<mean_theta<<","<<mean_phi<<","<<mean_psi<<",";
-    fout <<max_tx<<","<<max_ty<<","<<max_tz<<","<<max_theta<<","<<max_phi<<","<<max_psi<<endl;
+   
 
     
 }
@@ -133,18 +84,61 @@ int do_work( const string & htmlname , const string & sub_dir ){
         fhtml<<"<body>"<<endl;
         fhtml<<"Analysis Report for <a href=\"file://"<<sub_dir<<"\"> "<<sub_dir<<"</a> <br>"<<endl;
 
+//STRUCTURAL IMAGE
+        fhtml<<"<p> <b> T1-weighted Structural Image</b> <br>"<<endl;
+        fhtml<<"<a href=\""<<sub_dir+"/qc/structural_allaxial.png\" width=\"1000\""<<"\">"<<sub_dir+"/struct/orig "<<"</a>"<<endl;
+        fhtml<<"<img src=\""<<sub_dir+"/qc/structural.png\""<<" width=\"1000\" "<<">"<<endl;
+        fhtml<<"</p>"<<endl;
+        
+        fhtml<<"<p> <b> Mid Time Point Functional Image</b> <br>"<<endl;
+        fhtml<<"<a href=\""<<sub_dir+"/qc/example_func_allaxial.png\" width=\"1000\""<<"\">"<<sub_dir+"/example_func "<<"</a>"<<endl;
+        fhtml<<"<img src=\""<<sub_dir+"/qc/example_func.png\""<<" width=\"1000\" "<<">"<<endl;
+        fhtml<<"</p>"<<endl;
+
+        fhtml<<"<p> <b> Brain Extraction from Highres Structural</b> <br>"<<endl;
+        fhtml<<"<a href=\""<<sub_dir+"/qc/brain_extraction_allaxial.png\" width=\"1000\""<<"\">"<<sub_dir+"/struct/brain_fnirt_mask "<<"</a>"<<endl;
+        fhtml<<"<img src=\""<<sub_dir+"/qc/brain_extraction.png\""<<" width=\"1000\" "<<">"<<endl;
+        fhtml<<"</p>"<<endl;
+        
+        
+        fhtml<<"<p> <b> Registration of Functional Image to Structural </b> <br>"<<endl;
+        fhtml<<"<a href=\""<<sub_dir+"/qc/example_func2highres_allaxial.png\" width=\"1000\""<<"\">"<<sub_dir+"/reg/example_func_2_highres "<<"</a>"<<endl;
+        fhtml<<"<img src=\""<<sub_dir+"/qc/example_func2highres.png\""<<" width=\"1000\" "<<">"<<endl;
+        fhtml<<"</p>"<<endl;
+
+        fhtml<<"<p> <b> Registration of Structural Image to MNI152 </b> <br>"<<endl;
+        fhtml<<"<a href=\""<<sub_dir+"/qc/highres2standard_allaxial.png\" width=\"1000\""<<"\">"<<sub_dir+"/reg/example_func_2_highres "<<"</a>"<<endl;
+        fhtml<<"<img src=\""<<sub_dir+"/qc/highres2standard.png\""<<" width=\"1000\" "<<">"<<endl;
+        fhtml<<"</p>"<<endl;
+
+        
+        
+        
+        
         fhtml<<"<p> <b> Motion Plots</b> <br>"<<endl;
         
-        fhtml<<" <table border=\"1\" style=\"width:20%\">"<<endl;
+        string motion_pars_rel=sub_dir + "/mc/prefiltered_func_data_mcf_rel.rms";
+        string motion_pars_abs=sub_dir + "/mc/prefiltered_func_data_mcf_abs.rms";
+        float max_disp,mean_disp;
+        
+        unsigned int N_disp_gt_05;
+        calc_motion( motion_pars_abs, motion_pars_rel,mean_disp, max_disp, N_disp_gt_05 );
+        
+        
+        fhtml<<" <table border=\"1\" style=\"width:40%\">"<<endl;
          fhtml<<"<tr>"<<endl;
-         fhtml<<"<td>Mean RMS Displacment</td>"<<endl;
-         fhtml<<"<td>0</td>"<<endl;
+         fhtml<<"<td>Mean RMS Absolute Displacment</td>"<<endl;
+         fhtml<<"<td>"<<mean_disp<<"</td>"<<endl;
         fhtml<<"</tr>"<<endl;
 
         fhtml<<"<tr>"<<endl;
-         fhtml<<"<td>Maximnum RMS displacement</td>"<<endl;
-        fhtml<<"<td>0</td>"<<endl;
+         fhtml<<"<td>Maximnum RMS Absolute displacement</td>"<<endl;
+        fhtml<<"<td>"<<max_disp<<"</td>"<<endl;
 
+        fhtml<<"<tr>"<<endl;
+        fhtml<<"<td>Number of Relative Motions > 0.5mm</td>"<<endl;
+        fhtml<<"<td>"<<N_disp_gt_05<<"</td>"<<endl;
+        
          fhtml<<"</tr>"<<endl;
         fhtml<<"</table>"<<endl;
 
