accd5754339a019ec81a023fc18d7186d9309bdd
Brian M. Patenaude <brianpatenaude@mac.com>
2014-10-01 13:19:28 -0700

    merge

--- analysis_pipeline_qc.sh
+++ analysis_pipeline_qc.sh
@@ -1,41 +1,74 @@
 #!/bin/sh
 
-ANADIR=/Users/brian/susrc/analysis_pipeline/
+function Usage(){
+
+    echo "\n analysis_pipeline_qc.sh <raw_func_4D> <analysis directory>"
+
+}
+
+
+
+
+ANADIR=`dirname $0`
+#/Users/brian/susrc/analysis_pipeline/
+#echo $0
+
+
 
 
 function sliceImage()
 {
-pairimage=""
-if [ $1 = -p ]; then
-    pairimage=$2
-    shift 2
-fi
+    pairimage=""
+    SLCOPTS=""
+    PARSEOPTS=1;
+    while [ $PARSEOPTS = 1 ]; do
+	if [ $1 = -p ]; then
+	    pairimage=$2
+	    shift 2
+	elif [ $1 = -e ]; then
+	    SLCOPTS="${SLCOPTS} -e $2 "
+	    shift 2
+	else
+	    PARSEOPTS=0
+	fi
 
-A=`${FSLDIR}/bin/remove_ext $1`
-OUTPUT=`${FSLDIR}/bin/remove_ext $2`
+    done
 
-IMAGE_dir=${OUTPUT}_dir
+    A=`${FSLDIR}/bin/remove_ext $1`
+    OUTPUT=`${FSLDIR}/bin/remove_ext $2`
 
-mkdir ${IMAGE_dir}
-sliceropts="$edgeopts -x 0.4 ${IMAGE_dir}/grota.png -x 0.5 ${IMAGE_dir}/grotb.png -x 0.6 ${IMAGE_dir}/grotc.png -y 0.4 ${IMAGE_dir}/grotd.png -y 0.5 ${IMAGE_dir}/grote.png -y 0.6 ${IMAGE_dir}/grotf.png -z 0.4 ${IMAGE_dir}/grotg.png -z 0.5 ${IMAGE_dir}/groth.png -z 0.6 ${IMAGE_dir}/groti.png"
+    IMAGE_dir=${OUTPUT}_dir
 
-convertopts="${IMAGE_dir}/grota.png + ${IMAGE_dir}/grotb.png + ${IMAGE_dir}/grotc.png + ${IMAGE_dir}/grotd.png + ${IMAGE_dir}/grote.png + ${IMAGE_dir}/grotf.png + ${IMAGE_dir}/grotg.png + ${IMAGE_dir}/groth.png + ${IMAGE_dir}/groti.png"
+    mkdir ${IMAGE_dir}
+    sliceropts="$edgeopts -x 0.4 ${IMAGE_dir}/grota.png -x 0.5 ${IMAGE_dir}/grotb.png -x 0.6 ${IMAGE_dir}/grotc.png -y 0.4 ${IMAGE_dir}/grotd.png -y 0.5 ${IMAGE_dir}/grote.png -y 0.6 ${IMAGE_dir}/grotf.png -z 0.4 ${IMAGE_dir}/grotg.png -z 0.5 ${IMAGE_dir}/groth.png -z 0.6 ${IMAGE_dir}/groti.png"
+
+    convertopts="${IMAGE_dir}/grota.png + ${IMAGE_dir}/grotb.png + ${IMAGE_dir}/grotc.png + ${IMAGE_dir}/grotd.png + ${IMAGE_dir}/grote.png + ${IMAGE_dir}/grotf.png + ${IMAGE_dir}/grotg.png + ${IMAGE_dir}/groth.png + ${IMAGE_dir}/groti.png"
 
 
-${FSLDIR}/bin/slicer  $A $pairimage -s 1 $sliceropts
- ${FSLDIR}/bin/pngappend $convertopts ${OUTPUT}.png
+    ${FSLDIR}/bin/slicer  $A $pairimage $SLCOPTS -s 1 $sliceropts
+    ${FSLDIR}/bin/pngappend $convertopts ${OUTPUT}.png
 
-rm -r ${IMAGE_dir}
+    rm -r ${IMAGE_dir}
 
 
 }
 
 
-ANALYSISDIR=$1
+if [ $# = 0 ]; then
 
-#ANALYSISDIR=`readlink -f $1`
+    Usage;
+    exit 1
 
-QCDIR=${ANALYSISDIR}/qc
+fi
+
+
+
+RAW_FUNC=$1
+ANALYSISDIR=$2
+
+ANALYSISDIR=`readlink -f $2`
+
+QCDIR=${ANALYSISDIR}/report
 if [ ! -d ${QCDIR} ]; then
     mkdir ${QCDIR}
 fi
@@ -43,43 +76,68 @@ fi
 
 #do structural QC
 STRUCT=${ANALYSISDIR}/struct/orig
-
-if [ 1 = 0 ]; then
-echo "Slicing structural image..."
-sliceImage $STRUCT ${QCDIR}/structural
-slicer ${STRUCT} -s 1 -A 10000 ${QCDIR}/structural_allaxial.png
-
+FUNC2HIGHRES=${ANALYSISDIR}/reg/example_func2highres
+STD=${FSLDIR}/data/standard/MNI152_T1_2mm
+SUBCORT=${ANALYSISDIR}/struct/first_all_fast_firstseg.nii.gz
+BETMASK=${ANALYSISDIR}/struct/brain_fnirt_mask.nii.gz
+WARPED=${ANALYSISDIR}/reg/highres2standard_warped
 FUNC=${ANALYSISDIR}/example_func
-echo "Slicing functional image..."
-sliceImage $FUNC ${QCDIR}/example_func
-slicer ${FUNC} -s 1 -A 1000 ${QCDIR}/example_func_allaxial.png
+FAST=${ANALYSISDIR}/struct/brain_fnirt_pveseg.nii.gz
 
-FUNC=${ANALYSISDIR}/reg/example_func2highres
-echo "Slicing functional->structural registration image..."
-sliceImage -p $STRUCT $FUNC ${QCDIR}/example_func2highres
-slicer  ${FUNC} $STRUCT  $ -s 1 -A 10000 ${QCDIR}/example_func2highres_allaxial.png
 
-fi
+#if [ 0 = 1 ]; then
+    echo "Slicing structural image..."
+    sliceImage $STRUCT ${QCDIR}/structural
+    slicer ${STRUCT} -s 1 -A 10000 ${QCDIR}/structural_allaxial.png
+
+    echo "Slicing functional image..."
+    sliceImage $FUNC ${QCDIR}/example_func
+    slicer ${FUNC} -s 8 -A 10000 ${QCDIR}/example_func_allaxial.png
+
+    echo "Slicing functional->structural registration image..."
+    sliceImage -p $STRUCT $FUNC2HIGHRES ${QCDIR}/example_func2highres
+    slicer  ${FUNC2HIGHRES} $STRUCT  $ -s 1 -A 10000 ${QCDIR}/example_func2highres_allaxial.png
+
 
 #lets do structural to standard space
 #warp highres image
-STD=${FSLDIR}/data/standard/MNI152_T1_2mm
-applywarp -i $STRUCT -r $STD -w ${ANALYSISDIR}/reg/highres2standard_warp -o ${ANALYSISDIR}/reg/highres2standard_warped
-WARPED=${ANALYSISDIR}/reg/highres2standard_warped
-sliceImage -p $STD $WARPED ${QCDIR}/highres2standard
-slicer  ${WARPED} $STD  $ -s 1 -A 2000 ${QCDIR}/highres2standard_allaxial.png
+    echo "Warping structural to standard space and slicing"
+    applywarp -i $STRUCT -r $STD -w ${ANALYSISDIR}/reg/highres2standard_warp -o ${ANALYSISDIR}/reg/highres2standard_warped
+    sliceImage -p $STD $WARPED ${QCDIR}/highres2standard
+    slicer  ${WARPED} $STD  $ -s 1 -A 2000 ${QCDIR}/highres2standard_allaxial.png
 
 #DO BET
+    echo "Creating Brain Extraction Outline and Slicing"
+    sliceImage -p $BETMASK $STRUCT ${QCDIR}/brain_extraction
+    slicer  ${STRUCT} $BETMASK  $ -s 1 -A 2000 ${QCDIR}/brain_extraction_allaxial.png
 
-BETMASK=${ANALYSISDIR}/struct/brain_fnirt_mask.nii.gz
-sliceImage -p $BETMASK $STRUCT ${QCDIR}/brain_extraction
-slicer  ${STRUCT} $BETMASK  $ -s 1 -A 2000 ${QCDIR}/brain_extraction_allaxial.png
+#lets do the subcortical
+    echo "Creating Subcortical Outlines and Slicing"
+    sliceImage -e -0.5 -p $SUBCORT $STRUCT ${QCDIR}/firstseg
+    slicer  ${STRUCT} $SUBCORT  -e -0.5 -s 1 -A 2000 ${QCDIR}/firstseg_allaxial.png
+
+#lets do the subcortical
+    echo "Creating Tissue Segmentation Outlines and Slicing"
+    sliceImage -e -0.1 -p $FAST $STRUCT ${QCDIR}/fastpveseg
+    slicer  ${STRUCT} $FAST  -e -0.1 -s 1 -A 2000 ${QCDIR}/fastpveseg_allaxial.png
+
+#sSNR
+
+flirt -in ${ANALYSISDIR}/struct/brain_fnirt_mask -ref ${ANALYSISDIR}/example_func -applyxfm -init ${ANALYSISDIR}/reg/highres2example_func.mat -out ${ANALYSISDIR}/struct/brain_fnirt_mask_2_example_func
+
+#fi
+#fslmaths $ORIGFUNC -mas ${ANALYSISDIR}/struct/brain_fnirt_mask_2_example_func -Tstd  ${QCDIR}/func_std
+#fslmaths $ORIGFUNC -mas ${ANALYSISDIR}/struct/brain_fnirt_mask_2_example_func -Tmean  ${QCDIR}/func_mean
+#fslmaths ${QCDIR}/func_mean -div ${QCDIR}/func_std ${QCDIR}/vSNR
+
+#fslstats ${QCDIR}/vSNR -M > ${ANALYSISDIR}/vSNR.txt
 
 
-
+#copy motion p[lots
+cp ${ANALYSISDIR}/mc/*.png ${QCDIR}/
 
 #sliceImage $1 $2
 echo "Creating HTML ${QCDIR}/index.html "
-${ANADIR}/create_subject_report/create_subject_report ${QCDIR}/index $ANALYSISDIR
+${ANADIR}/create_subject_report/create_subject_report ${QCDIR}/index $RAW_FUNC $ANALYSISDIR
 
 
--- create_subject_report/create_subject_report.cpp
+++ create_subject_report/create_subject_report.cpp
@@ -20,7 +20,7 @@ using namespace std;
 void usage(){
     
     cout<<"\n Usage : "<<endl;
-    cout<<"create_subject_report <output_file> <list of analysis directories> \n"<<endl;
+    cout<<"create_subject_report <output_file> <raw 4D timeseries> <list of analysis directories> \n"<<endl;
     
 }
 void calc_motion( const string & fmotion_disp, const string & fmotion_disp_rel, float & mean_disp, float & max_disp, unsigned int &  N_disp_gt_05 )
@@ -70,7 +70,7 @@ void calc_motion( const string & fmotion_disp, const string & fmotion_disp_rel, 
     
 }
 
-int do_work( const string & htmlname , const string & sub_dir ){
+int do_work( const string & htmlname , const string & raw_func, const string & sub_dir ){
     ofstream fhtml(htmlname.c_str());
     if (fhtml.is_open())
     {
@@ -86,37 +86,232 @@ int do_work( const string & htmlname , const string & sub_dir ){
 
 //STRUCTURAL IMAGE
         fhtml<<"<p> <b> T1-weighted Structural Image</b> <br>"<<endl;
-        fhtml<<"<a href=\""<<sub_dir+"/qc/structural_allaxial.png\" width=\"1000\""<<"\">"<<sub_dir+"/struct/orig "<<"</a>"<<endl;
-        fhtml<<"<img src=\""<<sub_dir+"/qc/structural.png\""<<" width=\"1000\" "<<">"<<endl;
+        fhtml<<"<a href=\""<<"structural_allaxial.png\" width=\"1000\""<<">"<<sub_dir+"/struct/orig "<<"</a>"<<endl;
+        fhtml<<"<br>"<<endl;
+        fhtml<<"<img src=\""<<"structural.png\""<<" width=\"1000\" "<<">"<<endl;
         fhtml<<"</p>"<<endl;
         
         fhtml<<"<p> <b> Mid Time Point Functional Image</b> <br>"<<endl;
-        fhtml<<"<a href=\""<<sub_dir+"/qc/example_func_allaxial.png\" width=\"1000\""<<"\">"<<sub_dir+"/example_func "<<"</a>"<<endl;
-        fhtml<<"<img src=\""<<sub_dir+"/qc/example_func.png\""<<" width=\"1000\" "<<">"<<endl;
+        fhtml<<"<a href=\""<<"example_func_allaxial.png\" width=\"1000\""<<">"<<sub_dir+"/example_func "<<"</a>"<<endl;
+        fhtml<<"<br>"<<endl;
+        fhtml<<"<img src=\""<<"example_func.png\""<<" width=\"1000\" "<<">"<<endl;
         fhtml<<"</p>"<<endl;
 
         fhtml<<"<p> <b> Brain Extraction from Highres Structural</b> <br>"<<endl;
-        fhtml<<"<a href=\""<<sub_dir+"/qc/brain_extraction_allaxial.png\" width=\"1000\""<<"\">"<<sub_dir+"/struct/brain_fnirt_mask "<<"</a>"<<endl;
-        fhtml<<"<img src=\""<<sub_dir+"/qc/brain_extraction.png\""<<" width=\"1000\" "<<">"<<endl;
+        fhtml<<"<a href=\""<<"brain_extraction_allaxial.png\" width=\"1000\""<<">"<<sub_dir+"/struct/brain_fnirt_mask "<<"</a>"<<endl;
+        fhtml<<"<br>"<<endl;
+        fhtml<<"<img src=\""<<"brain_extraction.png\""<<" width=\"1000\" "<<">"<<endl;
         fhtml<<"</p>"<<endl;
         
         
         fhtml<<"<p> <b> Registration of Functional Image to Structural </b> <br>"<<endl;
-        fhtml<<"<a href=\""<<sub_dir+"/qc/example_func2highres_allaxial.png\" width=\"1000\""<<"\">"<<sub_dir+"/reg/example_func_2_highres "<<"</a>"<<endl;
-        fhtml<<"<img src=\""<<sub_dir+"/qc/example_func2highres.png\""<<" width=\"1000\" "<<">"<<endl;
+        fhtml<<"<a href=\""<<"example_func2highres_allaxial.png\" width=\"1000\""<<">"<<sub_dir+"/reg/example_func_2_highres "<<"</a>"<<endl;
+        fhtml<<"<br>"<<endl;
+        fhtml<<"<img src=\""<<"example_func2highres.png\""<<" width=\"1000\" "<<">"<<endl;
         fhtml<<"</p>"<<endl;
 
         fhtml<<"<p> <b> Registration of Structural Image to MNI152 </b> <br>"<<endl;
-        fhtml<<"<a href=\""<<sub_dir+"/qc/highres2standard_allaxial.png\" width=\"1000\""<<"\">"<<sub_dir+"/reg/example_func_2_highres "<<"</a>"<<endl;
-        fhtml<<"<img src=\""<<sub_dir+"/qc/highres2standard.png\""<<" width=\"1000\" "<<">"<<endl;
+        fhtml<<"<a href=\""<<"highres2standard_allaxial.png\" width=\"1000\""<<">"<<sub_dir+"/reg/highres2standard_warped "<<"</a>"<<endl;
+        fhtml<<"<br>"<<endl;
+        fhtml<<"<img src=\""<<"highres2standard.png\""<<" width=\"1000\" "<<">"<<endl;
         fhtml<<"</p>"<<endl;
 
         
+        fhtml<<"<p> <b> Subcortical Segmentation using FIRST in Structural Space </b> <br>"<<endl;
+        fhtml<<"<a href=\""<<"firstseg_allaxial.png\" width=\"1000\""<<">"<<sub_dir+"/struct/first_all_fast_firstseg "<<"</a>"<<endl;
+        fhtml<<"<br>"<<endl;
+        fhtml<<"<img src=\""<<"firstseg.png\""<<" width=\"1000\" "<<">"<<endl;
+        fhtml<<"</p>"<<endl;
         
         
+        fhtml<<"<p> <b> Tissue Segmentation using FAST in Structural Space </b> <br>"<<endl;
+        fhtml<<"<a href=\""<<"fastpveseg_allaxial.png\" width=\"1000\""<<">"<<sub_dir+"/struct/brain_fnirt_pveseg "<<"</a>"<<endl;
+        fhtml<<"<br>"<<endl;
+        fhtml<<"<img src=\""<<"fastpveseg.png\""<<" width=\"1000\" "<<">"<<endl;
+        fhtml<<"</p>"<<endl;
+        
+        //-------------let's do some SNR stuff--------------------
+//-------FOR SNR STUFF LET OMIT THE FIRST 5 volumes, because we throw away anyways
+        const unsigned int Ndiscard = 5 ;
+//        cout<<"start SNR stuff "<<raw_func<<endl;
+        volume4D<float> raw_fmri;
+        read_volume4D(raw_fmri,raw_func);
+
+        volume<float> SNR,sx,sxx;
+        volume<float> brain_mask;
+        read_volume(brain_mask,sub_dir+"/struct/brain_fnirt_mask_2_example_func");
+        SNR=sx=sxx=brain_mask;
+        SNR=0;
+        sx=0;
+        sxx=0;
+
+        int Nt=raw_fmri.tsize();
+        int xsize=raw_fmri.xsize();
+        int ysize=raw_fmri.ysize();
+        int zsize=raw_fmri.zsize();
+        
+//        cout<<"sizes "<<xsize<<" "<<sx.xsize();
+
+        
+//        cout<<"start loading the data"<<endl;
+        //claulctae mean and standard deviations
+        vector< vector<float> > mean_slice_signal(zsize, vector<float>());
+        vector<unsigned int> n_per_slice;
+        for (int t=Ndiscard; t<Nt ; t++)
+        {
+//            cout<<"timepoint "<<t<<endl;
+            const float* br_ptr =brain_mask.fbegin();
+            //const float* snr_ptr =brain_mask.fbegin();
+            const float* raw_ptr =(raw_fmri[t].fbegin());
+            float* sx_ptr =const_cast<float*>(sx.fbegin());
+            float* sxx_ptr =const_cast<float*>(sxx.fbegin());
+
+            //volume<float> v_t(xsize,ysize,zsize);
+            for (int z=0; z<zsize ; z++)
+            {
+                unsigned int Nslice=0;
+                float mean_slice=0;
+                for (int y=0; y<ysize ; y++)
+                    for (int x=0; x<xsize ; x++,raw_ptr++,br_ptr++,++sx_ptr,++sxx_ptr  )
+                    {
+                        if (*br_ptr>0)
+                        {
+                            *sx_ptr +=*raw_ptr;
+                            *sxx_ptr += (*raw_ptr)*(*raw_ptr);
+                            mean_slice += *raw_ptr;
+                            ++Nslice;
+                        }
+                    }
+                n_per_slice.push_back(Nslice);
+                mean_slice_signal[z].push_back(mean_slice/Nslice);
+                
+            }
+        }
+    
+//        //claculate ssnr
+        float ssnr=0;
+        {
+            unsigned int Ntotal=0;
+            float SNR_total=0;
+            unsigned int slice = 0 ;
+            vector< unsigned int >::iterator i_N = n_per_slice.begin();
+            for (vector< vector<float> >::iterator i = mean_slice_signal.begin(); i != mean_slice_signal.end(); ++i,++slice, ++i_N)
+            {
+//                cout<<"slice "<<slice<<" "<<i->size()<<" "<<*i_N<<endl;
+                if ( *i_N > 0 )
+                {
+                    float sxtemp=0;
+                    float sxxtemp=0;
+                    
+                    for (vector<float>::iterator ii = i->begin() ; ii != i->end(); ++ii)
+                    {
+                        sxtemp+=*ii;
+                        sxxtemp+=(*ii)*(*ii);
+                    }
+                    unsigned int N = i->size();
+                    
+//                    cout<<"Slice SNR "<<(sxtemp/N) / sqrt(( sxxtemp  - (sxtemp * sxtemp/N) ) / (N-1) )<<" "<<N<<endl;
+//                    SNR_total +=  N * (sx/N) /  sqrt(( sxx  - (sx * sx/N) ) / (N-1) );//i kbnoew there is a redundant N multiplicaton, but makes more obvious
+                    SNR_total += sxtemp /  sqrt(( sxxtemp  - (sxtemp * sxtemp/N) ) / (N-1) );//i kbnoew there is a redundant N multiplicaton, but makes more obvious
+
+                    Ntotal+=N;
+//                    cout<<"slice is NOT empty "<<SNR_total<<" "<<Ntotal<<endl;
+                    
+                }
+//                else{
+//                    cout<<"slice is empty "<<SNR_total<<" "<<Ntotal<<endl;
+//                }
+            }
+            SNR_total /=Ntotal;
+//            
+            ssnr = SNR_total;
+        }
+        
+//        cout<<"ssnr "<<ssnr<<endl;
+        Nt=Nt-Ndiscard;
+        SNR=sx/Nt;//mean
+        sxx= (sxx - sx*sx/Nt)/(Nt-1);
+        sxx=sqrt(sxx);
+
+        
+        
+        SNR/=sxx;
+        mask_volume(SNR,brain_mask);
+        save_volume(SNR,"SNR");
+        
+        float vsnr=0;//SNR.mean(brain_mask);
+
+        //calculate SNR
+        const float* br_ptr =brain_mask.fbegin();
+        unsigned int Nsnr=0;
+        for (int z=0; z<zsize ; z++)
+            for (int y=0; y<ysize ; y++)
+                for (int x=0; x<xsize ; x++,br_ptr++)
+                {
+                    if (*br_ptr>0)
+                    {
+                        vsnr+=SNR.value(x,y,z);
+                        ++Nsnr;
+                    }
+                }
+        vsnr/=Nsnr;
+        
+        
+                      fhtml<<"<p> <b> Signal-To-Noise Ratio</b> <br>"<<endl;
+        fhtml<<"<br> The following SNR values were performed after the deletion of the first 5 volumes, but without another processing of the time seriees. <br> The suggested interpretation of Slice-Based SNR (sSNR) is: <br><br>"<<endl;
+        
+        fhtml<<" <table border=\"1\" style=\"width:20%\">"<<endl;
+        fhtml<<"<tr>"<<endl;
+        fhtml<<"<td>Good</td>"<<endl;
+        fhtml<<"<td> =>150 </td>"<<endl;
+        fhtml<<"</tr>"<<endl;
+        fhtml<<"<tr>"<<endl;
+        fhtml<<"<td>Bad</td>"<<endl;
+        fhtml<<"<td> <99 </td>"<<endl;
+        fhtml<<"</tr>"<<endl;
+        fhtml<<"</table> <br>"<<endl;
+
+        fhtml<<" <table border=\"1\" style=\"width:40%\">"<<endl;
+        fhtml<<"<tr>"<<endl;
+        fhtml<<"<td>Voxel-based Signal to Noise Ratio (vSNR)</td>"<<endl;
+        fhtml<<"<td>"<<vsnr<<"</td>"<<endl;
+        fhtml<<"</tr>"<<endl;
+        fhtml<<"<tr>"<<endl;
+        fhtml<<"<td>Slice-based Signal to Noise Ratio (sSNR)</td>"<<endl;
+        fhtml<<"<td>"<<ssnr<<"</td>"<<endl;
+        fhtml<<"</tr>"<<endl;
+        fhtml<<"</table>"<<endl;
+
+        //------------------------------------------
+        
+
+                      fhtml<<"</p>"<<endl;
         
         fhtml<<"<p> <b> Motion Plots</b> <br>"<<endl;
+        fhtml<<"<br> The suggested interpretation of Maximum Absolute Motion is : <br><br>"<<endl;
+        fhtml<<" <table border=\"1\" style=\"width:20%\">"<<endl;
+        fhtml<<"<tr>"<<endl;
+        fhtml<<"<td>Good</td>"<<endl;
+        fhtml<<"<td> <1.49 </td>"<<endl;
+        fhtml<<"</tr>"<<endl;
+        fhtml<<"<tr>"<<endl;
+        fhtml<<"<td>Bad</td>"<<endl;
+        fhtml<<"<td> >2mm </td>"<<endl;
+        fhtml<<"</tr>"<<endl;
+        fhtml<<"</table> <br>"<<endl;
         
+        fhtml<<"<br> The suggested interpretation for Number of Movements > 0.5mm is : <br><br>"<<endl;
+        fhtml<<" <table border=\"1\" style=\"width:20%\">"<<endl;
+        fhtml<<"<tr>"<<endl;
+        fhtml<<"<td>Good</td>"<<endl;
+        fhtml<<"<td> <5 </td>"<<endl;
+        fhtml<<"</tr>"<<endl;
+        fhtml<<"<tr>"<<endl;
+        fhtml<<"<td>Bad</td>"<<endl;
+        fhtml<<"<td> >5 </td>"<<endl;
+        fhtml<<"</tr>"<<endl;
+        fhtml<<"</table> <br>"<<endl;
+
         string motion_pars_rel=sub_dir + "/mc/prefiltered_func_data_mcf_rel.rms";
         string motion_pars_abs=sub_dir + "/mc/prefiltered_func_data_mcf_abs.rms";
         float max_disp,mean_disp;
@@ -140,19 +335,23 @@ int do_work( const string & htmlname , const string & sub_dir ){
         fhtml<<"<td>"<<N_disp_gt_05<<"</td>"<<endl;
         
          fhtml<<"</tr>"<<endl;
-        fhtml<<"</table>"<<endl;
+        fhtml<<"</table> <br>"<<endl;
 
         
         
-        fhtml<<"<img src=\""<<sub_dir+"/mc/disp.png\""<<" alt=\"mean_disp\" width=\"747\" height=\"167\">"<<endl;
+        fhtml<<"<img src=\""<<"disp.png\""<<" alt=\"mean_disp\" width=\"747\" height=\"167\">"<<endl;
         fhtml<<"<br>"<<endl;
-        fhtml<<"<img src=\""<<sub_dir+"/mc/trans.png\""<<" alt=\"mean_trans\" width=\"747\" height=\"167\">"<<endl;
+        fhtml<<"<img src=\""<<"trans.png\""<<" alt=\"mean_trans\" width=\"747\" height=\"167\">"<<endl;
         fhtml<<"<br>"<<endl;
-        fhtml<<"<img src=\""<<sub_dir+"/mc/rot.png\""<<" alt=\"mean_rot\" width=\"747\" height=\"167\">"<<endl;
+        fhtml<<"<img src=\""<<"rot.png\""<<" alt=\"mean_rot\" width=\"747\" height=\"167\">"<<endl;
 
         fhtml<<"</p>"<<endl;
         fhtml<<"</body>"<<endl;
         fhtml<<"</html>"<<endl;
+        
+        
+        cout<<"Open report file in web browser:"<<endl;
+        cout<<htmlname<<endl;
 
     }
     return 0;
@@ -165,8 +364,9 @@ int main (int argc, const char * argv[])
         return 0;
     }
     string outname = string(argv[1]) + ".html";
-    string analysis_dir = argv[2];
-    do_work(outname,analysis_dir);
+    string rawfunc = argv[2];
+    string analysis_dir = argv[3];
+    do_work(outname,rawfunc,analysis_dir);
     
     return 0;
 }
